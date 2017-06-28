5ea25126318d8188756b76ed9102c934a83afa3e
leesangyong <cucrn35@gmail.com>
2017-06-28 18:40:05 +0900

    +

--- Class/src/Common/Exam2.java
+++ Class/src/Common/Exam2.java
@@ -11,6 +11,8 @@ import java.util.Scanner;
 
 public class Exam2 {
 
+public static List<String> hm;
+
 public static class Exam {
 	public List<String> getUserIDLists(String name){
 		//String result = "";
@@ -24,7 +26,7 @@ public static class Exam {
 			PreparedStatement prestmt = con.prepareStatement(sql);
 			ResultSet rs = prestmt.executeQuery();
 			while (rs.next()) {		
-				userlist.add(rs.getString(1) + "," + rs.getString(2) + "," + rs.getString(3));//()안에 숫자에 컬럼명 그대로 적어줘도 됨.
+				userlist.add(rs.getString(1) + "," + rs.getString(2) + "," + rs.getString(3) + "," +rs.getInt(4));
 			}			
 			DBConn2.closeCon();
 			return userlist;
@@ -36,15 +38,21 @@ public static class Exam {
 	
 	public boolean insertUser(){
 		try {
-			Connection con = DBConn2.getCon();	
+			Connection con = DBConn2.getCon();
 			Scanner scan = new Scanner(System.in);
-			HashMap<String, String> valueHm = new HashMap<String, String>();
-			valueHm.put("id", scan.nextLine());
-			valueHm.put("pwd", scan.nextLine());
-			valueHm.put("name", scan.nextLine());
-			valueHm.put("age", scan.nextLine());
+			HashMap<String, String> hm = new HashMap<String, String>();
+			System.out.println("id를 입력해주세요");
+			hm.put("id", scan.nextLine());
+			System.out.println("pwd를 입력해주세요");
+			hm.put("pwd", scan.nextLine());
+			System.out.println("name을 입력해주세요");
+			hm.put("name", scan.nextLine());
+			System.out.println("나이를 입력해주세요");
+			hm.put("age", scan.nextLine());
 			
-			String sql = "insert into user(id, pwd, name, age)values("+hm.get("id")+";
+			String sql = "insert into user(id, pwd, name, age)"
+					+ "values('" + hm.get("id") + "','" + hm.get("pwd") + "','" + hm.get("name") + "',"
+							+ Integer.parseInt(hm.get("age")) + ")";
 			PreparedStatement prestmt = con.prepareStatement(sql);
 			int result = prestmt.executeUpdate();
 			DBConn2.closeCon();
@@ -56,14 +64,33 @@ public static class Exam {
 		}
 		return false;
 	}
+	public boolean deleteUser(int num){
+		try {
+			Connection con = DBConn2.getCon();			
+			String sql = "delete from user where num=" + num;
+			PreparedStatement prestmt = con.prepareStatement(sql);
+			int result = prestmt.executeUpdate();
+			DBConn2.closeCon();
+			if(result>0){
+				return true;
+			}
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 	
 	public static void main(String[] args){
 		Exam e = new Exam();
-//		e.insertUser();
+//		if(e.insertUser()){
+//			System.out.println("오~ 잘들어갔네요 유저테이블에!!");
+//		}
+//		
+//		boolean isDel = e.deleteUser(1);
+//		if(isDel){
+//			System.out.println("유저테이블에 잘 삭제가 됬네요!!");
+//		}
 		List<String> userList = e.getUserIDLists("");
 		for(int i=0;i<userList.size();i++){
 			System.out.println(userList.get(i));
-		}
-	}
-}
-}
+		}}}}
