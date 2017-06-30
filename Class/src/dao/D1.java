377f7f974d7abb09040312bcf3a5979bb456b440
leesangyong <cucrn35@gmail.com>
2017-06-30 18:40:13 +0900

    2017 06 30

---
+++ Class/src/dao/D1.java
@@ -0,0 +1,5 @@
+package dao;
+
+public class D1 {
+
+}
--- Class/src/dao/TestDAO.java
+++ Class/src/dao/TestDAO.java
@@ -1,11 +1,12 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,50 +14,79 @@ import Common.DBConn2;
 
 public class TestDAO {
 
-	public boolean insertTest(){
+	public boolean insertTest() {
 		try {
 			Connection con = DBConn2.getCon();
 			String sql = "insert into test(TITLE, CONTENT, WRITER, REG_DATE)";
-					sql+= " values(?,?,?,?)"; 
+			sql += " values(?,?,?,?)";
 			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setString(1,  "게시물3");
-			ps.setString(2,  "내용3");
-			ps.setString(3,  "1");
+			ps.setString(1, "게시물3");
+			ps.setString(2, "내용3");
+			ps.setString(3, "1");
+			Date d = new Date();
+			SimpleDateFormat sdt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			ps.setString(4, sdt.format(d));
 			int result = ps.executeUpdate();
-			if(result==1){
+			if (result == 1) {
 				return true;
 			}
-		} catch (SQLException | ClassNotFoundException e) {
+			DBConn2.closeCon();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return false;
 	}
-	public List<Map> selectTest(){
+
+	public List<Map> selectTest() {
 		List<Map> testList = new ArrayList<Map>();
 		try {
 			Connection con = DBConn2.getCon();
+
+			String sql = "SELECT T.*,UI.ID, UI.NAME FROM TEST AS T, USER_INFO AS UI";
+			sql += " Where t.writer = ui.num;";
+			PreparedStatement ps = con.prepareStatement(sql);
 			
-			String sql="SELECT T.",UI.ID, UI.NAME FROM TEST AS T, USER_INFO AS UI";"
-					PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()){
+				Map hm = new HashMap();
+				hm.put("num", rs.getString("num"));
+				hm.put("title", rs.getString("title"));
+				hm.put("content", rs.getString("content"));
+				hm.put("writer", rs.getString("writer"));
+				hm.put("reg_date", rs.getString("reg_date"));
+				hm.put("id", rs.getString("id"));
+				hm.put("name", rs.getString("name"));
+				testList.add(hm);
+			}
+			DBConn2.closeCon();
+			return testList;
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
 		return null;
 	}
-	public boolean updateTest(){
+
+	public boolean updateTest() {
 		return true;
 	}
-	public boolean deleteTest(){
+
+	public boolean deleteTest() {
 		return true;
 	}
-	public static void main(String[]args){
-//		TestDAO tdao = new TestDAO();
-//		if(tdao.insertTest()){
-//			System.out.println("오~ 테스트테이블에 입력이 잘되었습니다.");
-//		}
-//		Date d = new Date();
-//		System.out.println(d.toString());
-//		SimpleDateFormat sdt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//		System.out.println(sdt.format(d));
-	}
+
+	public static void main(String[] args) {
+		 TestDAO tdao = new TestDAO();
+		 if(tdao.insertTest()){
+		 System.out.println("오~ 테스트테이블에 입력이 잘되었습니다.");
+		 }
+		 
+//		 List<Map> list = to.selectTest();
+//		 for(Map m : list){
+//			 System.out.println(m);
+		 }
+		// Date d = new Date();
+		// System.out.println(d.toString());
+		// SimpleDateFormat sdt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		// System.out.println(sdt.format(d));
+//	}
 }
